7f54c2e65ee34a794b7fd73b19245fd316f84deb
Manni Basi <manni.basi@gmail.com>
2024-12-02 02:08:03 +0000

    Ran calculator for part 1 / for first gold star

--- src/main/java/org/puzzles/Main.java
+++ src/main/java/org/puzzles/Main.java
@@ -2,6 +2,9 @@ package org.puzzles;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocationListReader reader = new LocationListReader("src/main/resources/day_1_puzzle_input");
+        DistanceCalculator calculator = new DistanceCalculator(reader.readListOne(), reader.readListTwo());
+        int distance = calculator.calculate();
+        System.out.println("Total distance: " + distance); //1223326
     }
 }
