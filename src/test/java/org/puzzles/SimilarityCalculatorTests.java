f71bce97e7a1a1bcbe59214bda17cf81c8a5e009
Manni Basi <manni.basi@gmail.com>
2024-12-02 02:34:52 +0000

    Added failing test for location that does appear in list (once)

--- src/test/java/org/puzzles/SimilarityCalculatorTests.java
+++ src/test/java/org/puzzles/SimilarityCalculatorTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SimilarityCalculatorTests {
     @Test
     public void testSimilarityScoreForItemThatDoesNotAppearInListTwo() {
@@ -21,5 +23,24 @@ public class SimilarityCalculatorTests {
         int similarityScore = calculator.calculate();
 
         assertEquals(0, similarityScore);
+    }
+
+    @Test
+    public void testSimilarityScoreForItemThatAppearsInListTwoOnce() {
+        List<Integer> locationIdListOne = new ArrayList<>();
+        locationIdListOne.add(3);
+        locationIdListOne.add(4);
+
+        List<Integer> locationIdListTwo = new ArrayList<>();
+        locationIdListTwo.add(4);
+        locationIdListTwo.add(7);
+        locationIdListTwo.add(9);
+        locationIdListTwo.add(7);
+
+        SimilarityCalculator calculator = new SimilarityCalculator(locationIdListOne, locationIdListTwo);
+
+        int similarityScore = calculator.calculate();
+
+        assertEquals(4, similarityScore);
     }
 }
