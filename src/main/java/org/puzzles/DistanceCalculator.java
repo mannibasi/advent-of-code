536f8bc03b0fd71b011738cd3a129c29e6678b6b
Manni Basi <manni.basi@gmail.com>
2024-12-02 00:17:21 +0000

    Added stubbed implementation to get test compiling / get feedback

---
+++ src/main/java/org/puzzles/DistanceCalculator.java
@@ -0,0 +1,12 @@
+package org.puzzles;
+
+import java.util.List;
+
+public class DistanceCalculator {
+    public DistanceCalculator(List<Integer> locationIdListOne, List<Integer> locationIdListTwo) {
+    }
+
+    public int calculate() {
+        return 0;
+    }
+}
