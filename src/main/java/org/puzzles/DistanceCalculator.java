d903665dffafd73b2867eb708810d67ffd632d37
Manni Basi <manni.basi@gmail.com>
2024-12-02 00:20:43 +0000

    Added simple(st) implementation / back to green

--- src/main/java/org/puzzles/DistanceCalculator.java
+++ src/main/java/org/puzzles/DistanceCalculator.java
@@ -3,10 +3,16 @@ package org.puzzles;
 import java.util.List;
 
 public class DistanceCalculator {
+
+    private final List<Integer> locationIdListOne;
+    private final List<Integer> locationIdListTwo;
+
     public DistanceCalculator(List<Integer> locationIdListOne, List<Integer> locationIdListTwo) {
+        this.locationIdListOne = locationIdListOne;
+        this.locationIdListTwo = locationIdListTwo;
     }
 
     public int calculate() {
-        return 0;
+        return locationIdListTwo.getFirst() - locationIdListOne.getFirst();
     }
 }
