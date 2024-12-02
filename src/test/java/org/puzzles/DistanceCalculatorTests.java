cd1a3a89346ba7bcc1ec6fd6ce8983eefffe62d2
Manni Basi <manni.basi@gmail.com>
2024-12-02 00:22:40 +0000

    Added test for multi item lists

--- src/test/java/org/puzzles/DistanceCalculatorTests.java
+++ src/test/java/org/puzzles/DistanceCalculatorTests.java
@@ -20,4 +20,20 @@ public class DistanceCalculatorTests {
 
         assertEquals(2, distance);
     }
+
+    @Test
+    public void testTotalDistanceCalculationWithMultiItemLists() {
+        List<Integer> locationIdListOne = new ArrayList<>();
+        locationIdListOne.add(1);
+        locationIdListOne.add(2);
+
+        List<Integer> locationIdListTwo = new ArrayList<>();
+        locationIdListTwo.add(3);
+        locationIdListTwo.add(4);
+
+        DistanceCalculator calculator = new DistanceCalculator(locationIdListOne, locationIdListTwo);
+        int distance = calculator.calculate();
+
+        assertEquals(4, distance);
+    }
 }
