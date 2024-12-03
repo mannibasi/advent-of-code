d843913d65102f27ff1f2694db83ffc4ca7cf7b3
Manni Basi <manni.basi@gmail.com>
2024-12-03 01:24:24 +0000

    Added logic to process report data / return list of levels

--- src/main/java/org/puzzles/rednosedreports/ReportProcessor.java
+++ src/main/java/org/puzzles/rednosedreports/ReportProcessor.java
@@ -1,12 +1,20 @@
 package org.puzzles.rednosedreports;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReportProcessor {
+
+    private final List<Integer> levels = new ArrayList<>();
+
     public ReportProcessor(String report) {
+        String[] levelReadings = report.split(" ");
+        for (String levelReading : levelReadings) {
+            levels.add(Integer.parseInt(levelReading));
+        }
     }
 
     public List<Integer> readLevels() {
-        return null;
+        return levels;
     }
 }
