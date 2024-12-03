89b003446a3c039be57f953ad718c2d1e0b95fe3
Manni Basi <manni.basi@gmail.com>
2024-12-03 00:22:47 +0000

    Implemented report line reader logic / back to green

---
+++ src/main/java/org/puzzles/rednosedreports/ReportReader.java
@@ -0,0 +1,23 @@
+package org.puzzles.rednosedreports;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReportReader {
+
+    private final List<String> lines;
+
+    public ReportReader(String filePath) {
+        try {
+            lines = Files.readAllLines(Path.of(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: " + filePath, e);
+        }
+    }
+
+    public List<String> readReportLines() {
+        return lines;
+    }
+}
--- src/test/java/org/puzzles/rednosedreports/ReportTests.java
+++ src/test/java/org/puzzles/rednosedreports/ReportTests.java
@@ -11,11 +11,12 @@ public class ReportTests {
     @Test
     public void testReadReportLine() {
         ReportReader reader = new ReportReader("src/test/resources/org/puzzles/rednosedreports/day_2_test_puzzle_input");
-        List<String> reportLine = reader.readReportLines();
+        List<String> reportLines = reader.readReportLines();
         assertAll(
-                () -> assertEquals("40 42 44 47 49 50 48", reportLine.get(0)),
-                () -> assertEquals("65 67 70 71 72 75 75", reportLine.get(1)),
-                () -> assertEquals("74 76 78 81 83 85 87 91", reportLine.get(2))
+                () -> assertEquals("40 42 44 47 49 50 48", reportLines.get(0)),
+                () -> assertEquals("65 67 70 71 72 75 75", reportLines.get(1)),
+                () -> assertEquals("74 76 78 81 83 85 87 91", reportLines.get(2))
         );
     }
+
 }
