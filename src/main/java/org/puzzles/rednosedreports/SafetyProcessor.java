284382fb2b8d1f222c3330b384c5fd39e11130ca
Manni Basi <manni.basi@gmail.com>
2024-12-03 02:07:19 +0000

    Added logic to detect level change of more than 3

--- src/main/java/org/puzzles/rednosedreports/SafetyProcessor.java
+++ src/main/java/org/puzzles/rednosedreports/SafetyProcessor.java
@@ -10,6 +10,9 @@ public class SafetyProcessor {
     }
 
     public boolean isSafe() {
+        if (adjacentValuesDifferByMoreThanThree()) {
+            return false;
+        }
         return allIncreasingOrDecreasing();
     }
 
@@ -27,5 +30,14 @@ public class SafetyProcessor {
         }
 
         return increasing || decreasing;
+    }
+
+    private boolean adjacentValuesDifferByMoreThanThree() {
+        for (int i = 1; i < levelReadings.size(); i++) {
+            if (Math.abs(levelReadings.get(i) - levelReadings.get(i - 1)) > 3) {
+                return true;
+            }
+        }
+        return false;
     }
 }
