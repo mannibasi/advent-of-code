44ff9265c26cf973881002f40734f4b2e023ef91
Manni Basi <manni.basi@gmail.com>
2024-12-03 01:21:31 +0000

    Fixed test compilation errors / stubbed implementation for feedback

---
+++ src/main/java/org/puzzles/rednosedreports/ReportProcessor.java
@@ -0,0 +1,12 @@
+package org.puzzles.rednosedreports;
+
+import java.util.List;
+
+public class ReportProcessor {
+    public ReportProcessor(String report) {
+    }
+
+    public List<Integer> readLevels() {
+        return null;
+    }
+}
