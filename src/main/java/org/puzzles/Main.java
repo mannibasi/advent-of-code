d8391732e6b273874e38f873a439f1a27b49044f
Manni Basi <manni.basi@gmail.com>
2024-12-03 02:28:54 +0000

    Added day 2 puzzle input data / processed it

--- src/main/java/org/puzzles/Main.java
+++ src/main/java/org/puzzles/Main.java
@@ -3,10 +3,31 @@ package org.puzzles;
 import org.puzzles.historianhysteria.DistanceCalculator;
 import org.puzzles.historianhysteria.LocationListReader;
 import org.puzzles.historianhysteria.SimilarityCalculator;
+import org.puzzles.rednosedreports.ReportProcessor;
+import org.puzzles.rednosedreports.ReportReader;
+import org.puzzles.rednosedreports.SafetyProcessor;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         historianHysteria();
+        redNosedReports();
+    }
+
+    private static void redNosedReports() {
+        ReportReader reader = new ReportReader("src/main/resources/org/puzzles/rednosedreports/day_2_puzzle_input");
+        List<String> reports = reader.readReports();
+        int safeReportCount = 0;
+        for (String report : reports) {
+            ReportProcessor processor = new ReportProcessor(report);
+            SafetyProcessor safetyProcessor = new SafetyProcessor(processor.readLevels());
+            if(safetyProcessor.isSafe()) {
+                safeReportCount++;
+            }
+        }
+        System.out.println("Safe report count: " + safeReportCount); //486
+
     }
 
     private static void historianHysteria() {
