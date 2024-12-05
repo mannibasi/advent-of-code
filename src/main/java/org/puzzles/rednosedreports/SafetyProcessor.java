9313b272ca8dbd78cb79ea8cd925d137aef2d378
= <manni.basi@gmail.com>
2024-12-05 23:07:02 +0000

    Refactored isSafe method to avoid repetition / encapsulated dampenerUsed

--- src/main/java/org/puzzles/rednosedreports/SafetyProcessor.java
+++ src/main/java/org/puzzles/rednosedreports/SafetyProcessor.java
@@ -5,112 +5,95 @@ import java.util.List;
 
 public class SafetyProcessor {
     private List<Integer> levelReadings;
-    private boolean dampenerUsed = false;
+    private final boolean mostlyIncreasing;
 
     public SafetyProcessor(List<Integer> levelReadings) {
         this.levelReadings = levelReadings;
-    }
-
-    private boolean isSafe(List<Integer> readings) {
-        if (adjacentValuesDifferByThreeOrLess(readings)) {
-            if (mostlyIncreasing()) {
-                return allIncreasing(readings);
-            }
-            return allDecreasing(readings);
-        }
-        return false;
+        this.mostlyIncreasing = mostlyIncreasing(levelReadings);
     }
 
     public boolean isSafe() {
-        if (adjacentValuesDifferByThreeOrLess(levelReadings)) {
-            if (mostlyIncreasing()) {
-                return allIncreasing(levelReadings);
+        return isSafe(levelReadings, false);
+    }
+
+    private boolean isSafe(List<Integer> readings, boolean dampenerUsed) {
+        if (adjacentValuesDifferByThreeOrLess(readings, dampenerUsed)) {
+            if (mostlyIncreasing) {
+                return allIncreasing(readings, dampenerUsed);
             }
-            return allDecreasing(levelReadings);
+            return allDecreasing(readings, dampenerUsed);
         }
         return false;
     }
 
-    private boolean mostlyIncreasing() {
+    private boolean mostlyIncreasing(List<Integer> readings) {
         int increasingCount = 0;
         int decreasingCount = 0;
-        for (int i = 1; i < levelReadings.size(); i++) {
-            if (levelReadings.get(i) > levelReadings.get(i - 1)) {
+        for (int i = 1; i < readings.size(); i++) {
+            if (readings.get(i) > readings.get(i - 1)) {
                 increasingCount++;
-            } else if (levelReadings.get(i) < levelReadings.get(i - 1)) {
+            } else if (readings.get(i) < readings.get(i - 1)) {
                 decreasingCount++;
             }
         }
         return increasingCount >= decreasingCount;
     }
 
-    private boolean allIncreasing(List<Integer> readings) {
+    private boolean allIncreasing(List<Integer> readings, boolean dampenerUsed) {
         for (int i = 1; i < readings.size(); i++) {
             if (readings.get(i) <= readings.get(i - 1)) {
-                if (dampenerUsed) {
-                    return false;
-                } else {
-                    dampenerUsed = true;
-                    return dampenedSafetyCheck(readings, i);
-                }
+                return dampenedSafetyCheck(readings, i, dampenerUsed);
             }
         }
         return true;
     }
 
-    private boolean allDecreasing(List<Integer> readings) {
+    private boolean allDecreasing(List<Integer> readings, boolean dampenerUsed) {
         for (int i = 1; i < readings.size(); i++) {
             if (readings.get(i) >= readings.get(i - 1)) {
-                if (dampenerUsed) {
-                    return false;
-                } else {
-                    dampenerUsed = true;
-                    return dampenedSafetyCheck(readings, i);
-                }
+                return dampenedSafetyCheck(readings, i, dampenerUsed);
             }
         }
         return true;
     }
 
-    private boolean adjacentValuesDifferByThreeOrLess(List<Integer> readings) {
+    private boolean adjacentValuesDifferByThreeOrLess(List<Integer> readings, boolean dampenerUsed) {
         for (int i = 1; i < readings.size(); i++) {
             if (Math.abs(readings.get(i) - readings.get(i - 1)) > 3) {
-                if (dampenerUsed) {
-                    return false;
-                } else {
-                    dampenerUsed = true;
-                    return dampenedSafetyCheck(readings, i);
-                }
+                return dampenedSafetyCheck(readings, i, dampenerUsed);
             }
         }
         return true;
     }
 
-    private boolean dampenedSafetyCheck(List<Integer> readings, int i) {
-        if (i >= 1) {
-            List<Integer> listWithPreviousItemRemoved = new ArrayList<>(readings);
-            listWithPreviousItemRemoved.remove(i - 1);
-            if (isSafe(listWithPreviousItemRemoved)) {
-                this.levelReadings = listWithPreviousItemRemoved;
-                return true;
-            }
+    private boolean dampenedSafetyCheck(List<Integer> readings, int index, boolean dampenerUsed) {
+        if (dampenerUsed) {
+            return false;
         }
 
-        if (readings.size() > i + 1) {
+        List<Integer> listWithPreviousItemRemoved = new ArrayList<>(readings);
+        listWithPreviousItemRemoved.remove(index - 1);
+        if (isSafe(listWithPreviousItemRemoved, true)) {
+            this.levelReadings = listWithPreviousItemRemoved;
+            return true;
+        }
+
+        if (readings.size() > index + 1) {
             List<Integer> listWithNextItemRemoved = new ArrayList<>(readings);
-            listWithNextItemRemoved.remove(i + 1);
-            if(isSafe(listWithNextItemRemoved)) {
+            listWithNextItemRemoved.remove(index + 1);
+            if (isSafe(listWithNextItemRemoved, true)) {
                 this.levelReadings = listWithNextItemRemoved;
                 return true;
-            };
+            }
         }
 
         List<Integer> listWithCurrentItemRemoved = new ArrayList<>(readings);
-        listWithCurrentItemRemoved.remove(i);
-        if(isSafe(listWithCurrentItemRemoved)) {
+        listWithCurrentItemRemoved.remove(index);
+        if (isSafe(listWithCurrentItemRemoved, true)) {
             this.levelReadings = listWithCurrentItemRemoved;
             return true;
-        };
+        }
+
         return false;
     }
 }
