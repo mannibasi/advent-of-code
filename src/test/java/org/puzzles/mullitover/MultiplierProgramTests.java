c04fc62d224803da97a4d1201110cfd757594bfa
Manni Basi <manni.basi@gmail.com>
2024-12-07 04:10:20 +0000

    Added failing / non-compiling test

--- src/test/java/org/puzzles/mullitover/MultiplierProgramTests.java
+++ src/test/java/org/puzzles/mullitover/MultiplierProgramTests.java
@@ -2,6 +2,8 @@ package org.puzzles.mullitover;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MultiplierProgramTests {
@@ -15,5 +17,12 @@ public class MultiplierProgramTests {
     public void testMultiplicationWithThreeDigitMultiplicandAndOneDigitMultiplier() {
         Calculator calculator = new Calculator();
         assertEquals(404, calculator.calculate("mul(101,4)"));
+    }
+
+    @Test
+    public void testCorruptedMemoryInstructionProcessorReturnsMultiplicationInstructions() {
+        CorruptedInstructionProccessor processor = new CorruptedInstructionProccessor("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+        List<String> instructions = processor.getMultiplicationInstructions();
+        assertEquals(4, instructions.size());
     }
 }
