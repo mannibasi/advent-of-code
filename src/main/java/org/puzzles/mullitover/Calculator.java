2dec985d1a046e64addb15923a1d0e77e94fff85
Manni Basi <manni.basi@gmail.com>
2024-12-07 03:45:29 +0000

    Initial obvious implementation / back to green

--- src/main/java/org/puzzles/mullitover/Calculator.java
+++ src/main/java/org/puzzles/mullitover/Calculator.java
@@ -2,6 +2,8 @@ package org.puzzles.mullitover;
 
 public class Calculator {
     public int calculate(String calculationInstruction) {
-        return 0;
+        int multiplicand = Integer.parseInt(calculationInstruction.substring(4, 5));
+        int multiplier = Integer.parseInt(calculationInstruction.substring(6, 7));
+        return multiplicand * multiplier;
     }
 }
