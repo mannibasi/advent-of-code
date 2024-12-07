e1aad2039d3f02df82ddcb6a01923df76da4acc1
Manni Basi <manni.basi@gmail.com>
2024-12-07 03:57:58 +0000

    Refactored multiplicand / multiplier assignment

--- src/main/java/org/puzzles/mullitover/Calculator.java
+++ src/main/java/org/puzzles/mullitover/Calculator.java
@@ -2,8 +2,9 @@ package org.puzzles.mullitover;
 
 public class Calculator {
     public int calculate(String calculationInstruction) {
-        int multiplicand = Integer.parseInt(calculationInstruction.substring(4, 5));
-        int multiplier = Integer.parseInt(calculationInstruction.substring(6, 7));
+        String[] instructionInput = calculationInstruction.replace("mul(", "").replace(")", "").split(",", 2);
+        int multiplicand = Integer.parseInt(instructionInput[0]);
+        int multiplier = Integer.parseInt(instructionInput[1]);
         return multiplicand * multiplier;
     }
 }
