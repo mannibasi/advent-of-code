0edf7054cde3809ccf9af39eb83fbee36633b297
Manni Basi <manni.basi@gmail.com>
2024-12-07 03:57:03 +0000

    Added failing test for multiple digit multiplication

--- src/test/java/org/puzzles/mullitover/MultiplierProgramTests.java
+++ src/test/java/org/puzzles/mullitover/MultiplierProgramTests.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MultiplierProgramTests {
     @Test
-    public void testMultiplication() {
+    public void testMultiplicationWithOneDigitMultiplicandAndMultiplier() {
         Calculator calculator = new Calculator();
         assertEquals(8, calculator.calculate("mul(2,4)"));
+    }
+
+    @Test
+    public void testMultiplicationWithThreeDigitMultiplicandAndOneDigitMultiplier() {
+        Calculator calculator = new Calculator();
+        assertEquals(404, calculator.calculate("mul(101,4)"));
     }
 }
