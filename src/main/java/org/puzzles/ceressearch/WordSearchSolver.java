113e5bf0823ce0a584b56c7d0cf60accd7c7d473
= <manni.basi@gmail.com>
2024-12-07 15:17:54 +0000

    Fixed test compilation error(s)

---
+++ src/main/java/org/puzzles/ceressearch/WordSearchSolver.java
@@ -0,0 +1,13 @@
+package org.puzzles.ceressearch;
+
+public class WordSearchSolver {
+    private final char[][] board;
+
+    public WordSearchSolver(char[][] board) {
+        this.board = board;
+    }
+
+    public int find(String word) {
+        return 0;
+    }
+}
