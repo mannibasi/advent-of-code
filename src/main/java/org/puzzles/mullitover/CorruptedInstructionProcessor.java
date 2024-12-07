b37187d4a8b8281bce1d2abdb0c9b27600e54e73
Manni Basi <manni.basi@gmail.com>
2024-12-07 04:29:33 +0000

    Fixed test compilation error(s)

---
+++ src/main/java/org/puzzles/mullitover/CorruptedInstructionProcessor.java
@@ -0,0 +1,15 @@
+package org.puzzles.mullitover;
+
+import java.util.List;
+
+public class CorruptedInstructionProcessor {
+    private final String corruptedInstructions;
+
+    public CorruptedInstructionProcessor(String corruptedInstructions) {
+        this.corruptedInstructions = corruptedInstructions;
+    }
+
+    public List<String> getMultiplicationInstructions() {
+        return null;
+    }
+}
