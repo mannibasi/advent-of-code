4e1ea69c7b424f4b02799be43aec6fad76cb4656
Manni Basi <manni.basi@gmail.com>
2024-12-10 21:10:57 +0000

    Added test for board from example puzzle

--- src/test/java/org/puzzles/ceressearch/WordSearchSolverTests.java
+++ src/test/java/org/puzzles/ceressearch/WordSearchSolverTests.java
@@ -76,4 +76,22 @@ public class WordSearchSolverTests {
         WordSearchSolver wordSearchSolver = new WordSearchSolver(board);
         assertEquals(1, wordSearchSolver.find("XMAS"));
     }
+
+    @Test
+    public void testExamplePuzzle() {
+        char[][] board = {
+                {'M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'},
+                {'M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'},
+                {'A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'},
+                {'M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'},
+                {'X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'},
+                {'X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'},
+                {'S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'},
+                {'S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'},
+                {'M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'},
+                {'M', 'X', 'M', 'X', 'A', 'M', 'A', 'S', 'X', 'X'}
+        };
+        WordSearchSolver wordSearchSolver = new WordSearchSolver(board);
+        assertEquals(18, wordSearchSolver.find("XMAS"));
+    }
 }
