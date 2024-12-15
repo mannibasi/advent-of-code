11bbf01338ab67c164f8193a56abfb6fe16121d2
= <manni.basi@gmail.com>
2024-12-15 15:45:59 +0000

    Fixed test compilation error(s)

---
+++ src/main/java/org/puzzles/printqueue/PageSorter.java
@@ -0,0 +1,13 @@
+package org.puzzles.printqueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PageSorter {
+    public PageSorter(Integer[][] sortingRules) {
+    }
+
+    public List<Integer> sortPages(LinkedList<Integer> pagesUpdates) {
+        return null;
+    }
+}
